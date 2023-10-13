2a2b750cad3e985ee94a5cb08e332359f4a68a1f
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-10-13 08:00:44 +0100

    updated resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService, UserData } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): UserData[] {
+    return this.appService.getUsers();
+  }
+
+  @Get(':name')
+  getUser(@Param('name') name: string) {
+    return this.appService.getSingleUser(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+export interface UserData {
+  name: string;
+  age: number;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers(): UserData[] {
+    const users = [
+      { name: 'Benjamin Nkem', age: 18 },
+      { name: 'Tochi Junior', age: 20 },
+      { name: 'Michael', age: 20 },
+    ];
+
+    return users;
+  }
+  getSingleUser(name: string): UserData {
+    const filteredUser = this.getUsers().filter(
+      (user) => user.name === name,
+    )[0];
+
+    return filteredUser;
   }
 }
